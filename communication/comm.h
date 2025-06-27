07a5b09abf39fcb4b28a3748b4e5bd1815140683
ElonYao <yao19888@gmail.com>
2025-06-27 09:54:12 -0400

    add header file for communication feature

--- communication/comm.h
+++ communication/comm.h
@@ -6,10 +6,15 @@
 #include "FreeRTOS.h"
 #include "queue.h"
 #include "task.h"
-
+#include "string.h"
+#include <ctype.h>
+#include <stdlib.h>
 typedef struct {
-    uint16_t command[5];
-    uint16_t value[10];
+    uint16_t dataBuffer[10];
+    uint16_t bufIndex;
+    uint32_t command;
+    float32_t parameter;
+    uint16_t tokens[2];
 }rcvd_t;
 //create task
 void SCITaskInit(void);
